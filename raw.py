21cb6328432987f44642fa394794b9ed96f3585b
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-12-09 01:12:30 +0100

    Create raw.py

---
+++ raw.py
@@ -0,0 +1,34 @@
+from typing import Any
+import asyncio
+
+class CustomWaitFor:
+    def __init__(self):
+      self._loop = asyncio.get_event_loop()
+      self._active_futures: dict[str, asyncio.Future] = {}
+
+    def new(self, tag: str) -> asyncio.Future:
+        f = asyncio.Future(loop=self._loop)
+        self._active_futures[tag] = f
+        return f
+
+    async def wait_for(self, tag: str,timeout: int = None) -> Any:
+        future = self._active_futures.get(tag)
+
+        try:
+            return await asyncio.wait_for(future, timeout=timeout)
+        except asyncio.TimeoutError:
+            return None
+        finally:
+            self._active_futures.pop(tag)
+
+    def set_result(self, tag: str, value: Any) -> None:
+        f = self._active_futures.get(tag)
+        f.set_result(value)
+
+    @property
+    def loop(self):
+        return self._loop
+    
+    @loop.setter
+    def loop(self, EventLoop: asyncio.AbstractEventLoop):
+        self._loop = EventLoop
