7e3f8aac9e3dea4894cd31d0c31f1c67db848a20
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-12-17 14:10:39 +0100

    R redundant method

--- raw.py
+++ raw.py
@@ -1,19 +1,19 @@
 from typing import Any
 import asyncio
 
-# WIP
 class CustomWaitFor:
     def __init__(self):
       self._loop = asyncio.get_event_loop()
       self._active_futures: dict[str, asyncio.Future] = {}
 
-    def new(self, tag: str) -> asyncio.Future:
-        f = asyncio.Future(loop=self._loop)
-        self._active_futures[tag] = f
-        return f
+    # def new(self, tag: str) -> asyncio.Future:
+    #     future = asyncio.Future(loop=self._loop)
+    #     self._active_futures[tag] = future
+    #     return future
 
     async def wait_for(self, tag: str, timeout: int = None) -> Any:
-        future = self._active_futures.get(tag)
+        future = asyncio.Future(loop=self._loop)
+        self._active_futures[tag] = future
 
         try:
             return await asyncio.wait_for(future, timeout=timeout)
@@ -23,8 +23,8 @@ class CustomWaitFor:
             self._active_futures.pop(tag)
 
     def set_result(self, tag: str, value: Any) -> None:
-        f = self._active_futures.get(tag)
-        f.set_result(value)
+        future = self._active_futures.get(tag)
+        future.set_result(value)
 
     @property
     def loop(self):
